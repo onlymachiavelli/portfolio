830697eef5e4c2b4eee3df1b5298cdd28a996e3c
Machiavelli <makaveli643@gmail.com>
2021-07-22 13:43:55 +0100

    Added an alert

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import React, {useEffect} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from './../public/FrontEnd/home/navbar'
 import Info from './../public/FrontEnd/home/info'
 export default function Home() {
+  useEffect(() =>{
+    alert("Website Building In Progress !")
+  })
   return (
     
   <main className="home">
