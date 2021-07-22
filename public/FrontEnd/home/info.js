aa6a98779fdcd49b20ed1ba2311b75dd0e68c56c
Machiavelli <makaveli643@gmail.com>
2021-07-22 13:41:03 +0100

    Fixing Info Part

--- public/FrontEnd/home/info.js
+++ public/FrontEnd/home/info.js
@@ -5,13 +5,16 @@ import kermit from './../../img/frog.png'
 const Info = () =>{
     let sym = ">"
     return (
-        <div className= "info">
+        <div className= "info" style={{textAlign:"center"}}>
             <div className="cntt">
                 <div className="image">
                     <Image src={kermit} alt="kermit"  width={"200"} height={"300"}/>
                 </div>
                 <div className="inf">
-                    <p id="title">{sym}_</p>
+                    <p id="title">{sym}_ Hi ! My name is @MACHIAVELLI</p>
+                    <p id="desc">
+                    My real name is Alaaddin, I’am self taught programmer and future software engineer, Im intrested in Space, Fly me to the mo... hahah, I play Guitar but I don't Sing lmao , 
+                    </p>
                 </div>
             </div>
         </div>
