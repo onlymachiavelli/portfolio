9897e4bf282c80e43af319a2a404717fc062cb31
Machiavelli <makaveli643@gmail.com>
2021-07-22 11:59:00 +0100

    Added SVG

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    
+  <main className="home">
+    
+  </main>
+  )
+}
