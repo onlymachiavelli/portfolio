e15c75658334dc0ccdc478b0a17778d0d14e4391
onlymachiavelli <tmakaveli643@gmail.com>
2021-07-24 01:07:32 +0100

    Only Links !not responsie

---
+++ public/packs/colors.js
@@ -0,0 +1,31 @@
+const Stuffs = {
+    facebook: {
+        color: "#486CB4",
+    },
+    instagram: {
+        color: "#C93C7D",
+    },
+
+    twitter: {
+        color: "#1C9CEA",
+    },
+    linkedin: {
+        color: "#0B66C3",
+    },
+    youtube: {
+        color: "#de0f0f",
+        href: ""
+    },
+    pinterest: {
+        color: "#E60023",
+    },
+    gmail: {
+        color: "#CA3A2D",
+        href: "",
+    },
+    github: {
+        color: "#211F1F",
+
+    }
+}
+export default Stuffs
