7c541a9d5e7289057b67700cd54ab6e910af9e13
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-31 21:52:19 +0100

    nav

--- src/useFront/Navbar.tsx
+++ src/useFront/Navbar.tsx
@@ -7,11 +7,23 @@ const Navbar = ({...props}) =>{
         <nav className={styles.navbar}>
             <div className={styles.logo}>
                 <Link href="/">
-                    <a> <Logo Width="40" Height="40" />
-                        
-                        <Machiavelli Width="150" Height="40" /></a>
+                    <a>
+                        <Logo Width="40" Height="40" />
+                        <Machiavelli Width="150" Height="40" />
+                    </a>
                 </Link>
             </div>
+            <nav className={styles.nav}>
+                <Link href="/">
+                    <a  className={styles.current_page}>
+                        Home
+                    </a>
+                </Link>
+                <Link href="/">skills</Link>
+                <Link href="/">Projects</Link>
+                <Link href="/">Works</Link>
+                <Link href="/">GetHelped</Link>
+            </nav>
 
         </nav>
     )
