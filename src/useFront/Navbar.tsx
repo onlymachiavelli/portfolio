24344bef6e2eac74b33a922a6489da177a12c02d
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-01 21:39:50 +0100

    nv

--- src/useFront/Navbar.tsx
+++ src/useFront/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react"
 import Link from 'next/link'
 import styles from './../../styles/Home.module.css'
-import {Machiavelli, Logo, Instagram, Twitter , Linkedin} from './icons'
+import {Machiavelli, Logo, GoldLinkedin, GoldTwitter, GoldInsta} from './icons'
 const Navbar = ({...props}) =>{
     return (
         <nav className={styles.navbar}>
@@ -25,7 +25,9 @@ const Navbar = ({...props}) =>{
                 <Link href="/">GetHelped</Link>
             </nav>
             <div className={styles.socialMedia}>
-
+                <a href=""><GoldInsta Width="21" Heght="21" /></a>
+                <a href=""><GoldTwitter Width="21" Height = "21" /></a>
+                <a href=""><GoldLinkedin Width="21" Height="21" /></a>
             </div>
         </nav>  
     )
