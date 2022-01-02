ff5003d5fcc26ea6e3b5cd20fa8345341b7b48bf
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-03 00:04:59 +0100

    .

--- src/useFront/home.tsx
+++ src/useFront/home.tsx
@@ -3,11 +3,12 @@ import React, { useState, useEffect} from 'react'
 
 import styles from './../../styles/Home.module.css'
 import Navbar from './Navbar'
-
+import HomeContainer from './mainHome'
 const Home = ({...props}) =>{
     return (
         <main className={styles.main_container}>
             <Navbar/>
+            <HomeContainer/>
         </main>
     )
 }
--- src/useFront/mainHome.tsx
+++ src/useFront/mainHome.tsx
@@ -1,10 +1,17 @@
 import React,{} from 'react'
 import styles from './../../styles/Home.module.css'
-
+import {MainLogo} from './icons'
 const HomeContainer = () =>{
     return(
         <main className={styles.homeContainer}>
-            
+            <div className={styles.homeTxt}>
+
+            </div>
+            <div className={styles.homeSvg}>
+                <MainLogo/>
+            </div>
         </main>
     )
 }
+
+export default HomeContainer
