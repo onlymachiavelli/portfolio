bfef75f1b333f6be390ad8ca4c22f88bacf783de
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-19 22:08:36 +0100

    text descr and name

--- src/useFront/mainHome.tsx
+++ src/useFront/mainHome.tsx
@@ -5,7 +5,13 @@ const HomeContainer = () =>{
     return(
         <main className={styles.homeContainer}>
             <div className={styles.homeTxt}>
-
+                <p id={styles.name}>{">"}_HI Im AlaaDdin</p>
+                <p id={styles.description}>
+                Self taught Programmer, 
+                Who’s hoping to go to space 
+                one day
+                </p>
+                <button className={styles.hireBtn}>Hire Me</button>
             </div>
             <div className={styles.homeSvg}>
                 <Rocket Width="350" Height="auto" />
