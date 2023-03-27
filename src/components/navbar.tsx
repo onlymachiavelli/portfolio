edf2df903f67f2b69b819a7d1875ff967953ed65
Alaa Barka <tmakaveli643@gmail.com>
2023-03-27 12:09:09 +0000

    preparing framermotion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,26 @@
 import styles from './../../styles/Home.module.css'
 import { Logo } from "./svg"
 import Link from "next/link"
+import {motion} from 'framer-motion'
 const Navbar = () =>{
     return (
-        <header className='flex'>
+        <header className='flex overflow-hidden w-full'>
             <nav>
                 <Link href={"/"}>
-                    <a><Logo Width={"150"}/></a>
+                    <motion.div 
+                        initial={{ scale: 0 }}
+                        animate={{ rotate: 360, scale: 1 }}>
+                        
+
+                        <a href='#' >
+                        <Logo Width={"150"}/></a>
+                    </motion.div>
                 </Link>
             </nav>
+            
 
-            <nav className={`${styles.nav} m-1.5 ml-10 `}>
-                <Link href={""}><a href="">HOME</a></Link>
+            <nav className={`${styles.nav} m-1.5 ml-10 pb-5 overflow-hidden `}>
+                <Link href={""}><a href="" className={`font-semiBold ${styles.current_page}`}>HOME</a></Link>
                 <Link href={""}><a href="">SERVICES</a></Link>
                 <Link href={""}><a href="">PORTFOLIO</a></Link>
                 <Link href={""}><a href="">MY WORKS</a></Link>
