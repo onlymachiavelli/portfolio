bd2cd427f9b1036b5b80254f85c9a9fc624b279a
Alaa Barka <tmakaveli643@gmail.com>
2023-04-15 07:25:40 +0100

    responsive contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,13 +8,13 @@ const Contact = () => {
                 --LEAVE A NICE MESSAGE
             </p>
 
-            <div className='flex items-center justify-center'>
-              <aside className='text-center w-1/3 flex  items-center justify-center flex-col gap-10'>
+            <div className='tablet:flex tablet:items-center tablet:justify-center w-full'>
+              <aside className='text-center tablet:w-1/3 w-full flex  items-center justify-center flex-col gap-10'>
                   <Duck Width="250" Height="auto" />
                   <p className='text-red font-bold text-xl text-center'>SEND DUDES</p>
               </aside>
 
-              <aside className=' text-center w-1/2 '>
+              <aside className=' text-center tablet:w-1/2 w-full '>
                   <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4">
                   <div className="mb-2">
 
