b88b3d200b8f1c527ae80e1a6a1ed51107daa277
Alaa Barka <tmakaveli643@gmail.com>
2023-06-12 05:46:21 +0100

    fixing smthng

--- src/components/Expret.tsx
+++ src/components/Expret.tsx
@@ -9,7 +9,7 @@ const Expert = () => {
                 --EXPER INT
                 </p>
             <p className='text-white text-sm m-auto w-auto text-center py-10'>I’VE BEEN TRYING NEW THINGS IN MY LIFE, AND IM USED TO LEARN AND TRY NEW AWESOME THINGS , SO I CAN 
-DEVELOP MY PERSONALITY AND ESPACE THE FUCKING MATRIX</p>
+DEVELOP MY PERSONALITY AND ESPACE THE MATRIX</p>
 
 
                <div className='w-full h-auto tablet:flex  tablet:space-x-8  items-center justify-center'>
@@ -17,7 +17,7 @@ DEVELOP MY PERSONALITY AND ESPACE THE FUCKING MATRIX</p>
                     <Card Logo={<Pen Width="50" />} Color="[#292929]" >
                         <p className='text-white font-bold py-2 text-center pb-4'>UI-UX DESIGN</p>
                         <p className='text-white w-full block px-2 '>
-                            I’VE BEEN PROTOTYPING 
+                            I'VE BEEN PROTOTYPING 
                             AND DESIGNING WEBSITES AND LOGOS 
                             AND MAKING AWESOME VIDEO EDITS
                             6 YEARS PLUS.
@@ -27,7 +27,7 @@ DEVELOP MY PERSONALITY AND ESPACE THE FUCKING MATRIX</p>
                     <Card Logo={<Brackets Width="50" />} Color="red" Margin={true}>
                         <p className='text-white font-bold py-2 text-center pb-4'>WEB DEVELOPEMENT</p>
                         <p className='text-white w-full block px-2 '>
-                            I’VE BEEN CREATING WEBSITES SINCE 
+                            I'VE BEEN CREATING WEBSITES SINCE 
                             2016 USING HTML CSS JS PHP, 
                             NOW IM FULL STACK, FRONT, BACKEND
                             USING MULTIPLE FRAMEWORKS AND
